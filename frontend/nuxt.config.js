7f3656b430220e12965ae03c3dd296d6f24578ab
Otero <oteronickbrian@gmail.com>
2024-07-03 23:35:40 +0300

    frontend components

---
+++ frontend/nuxt.config.js
@@ -0,0 +1,79 @@
+
+export default {
+  /*
+  ** Nuxt rendering mode
+  ** See https://nuxtjs.org/api/configuration-mode
+  */
+  mode: 'spa',
+  /*
+  ** Environment variables
+  ** See https://nuxtjs.org/api/configuration-env/
+  */
+  env: {
+    webClientId: process.env.WEB_CLIENT_ID || '',
+    userPoolId: process.env.USER_POOL_ID || 'us-east-2_fB1o1P4TV',
+    cognitoRegion: process.env.COGNITO_REGION || '',
+  },
+  /*
+  ** Nuxt target
+  ** See https://nuxtjs.org/api/configuration-target
+  */
+  target: 'static',
+  /*
+  ** Headers of the page
+  ** See https://nuxtjs.org/api/configuration-head
+  */
+  head: {
+    title: 'Voting Serverless App',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'Place your votes!' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+  /*
+  ** Global CSS
+  */
+  css: [
+  ],
+  /*
+  ** Plugins to load before mounting the App
+  ** https://nuxtjs.org/guide/plugins
+  */
+  plugins: [
+    { src: "~/plugins/amplify.js", mode: "client" }, "~/plugins/auth"
+  ],
+  /*
+  ** Auto import components
+  ** See https://nuxtjs.org/api/configuration-components
+  */
+  components: true,
+  /*
+  ** Nuxt.js dev-modules
+  */
+  buildModules: [
+  ],
+  /*
+  ** Nuxt.js modules
+  */
+  modules: [
+    // Doc: https://buefy.github.io/#/documentation
+    'nuxt-buefy',
+    // Doc: https://axios.nuxtjs.org/usage
+    '@nuxtjs/axios',
+  ],
+  /*
+  ** Axios module configuration
+  ** See https://axios.nuxtjs.org/options
+  */
+  axios: { baseURL: 'https://api.vote.fadhil-blog.dev' },
+  /*
+  ** Build configuration
+  ** See https://nuxtjs.org/api/configuration-build/
+  */
+  build: {
+  }
+}
