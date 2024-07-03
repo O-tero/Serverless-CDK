019d8b56b9ec7f3f2c72968d9b1b50456ad92865
Otero <oteronickbrian@gmail.com>
2024-07-03 23:40:40 +0300

    assets/components

---
+++ frontend/components/PollResult.vue
@@ -0,0 +1,24 @@
+<template>
+  <section class="section">
+    <div class="columns is-mobile">
+      <div class="content">
+        <h2 class="is-medium">{{ poll.question }}</h2>
+
+        <ul>
+          <li v-for="(count, answer) in poll.result"> {{ answer }} : {{ count }} </li>
+        </ul>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  props: {
+    poll: {
+      type: Object,
+      required: true,
+    },
+  }
+}
+</script>
